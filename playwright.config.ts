15b628163c363d815739db316ca49f1e46342ac9
Will Kelly <willkelly@Will-Kellys-Pro.local>
2023-01-20 17:01:38 -0600

    still working on getting tests in workflow file

--- playwright.config.ts
+++ playwright.config.ts
@@ -97,16 +97,18 @@ const config: PlaywrightTestConfig = {
     //     channel: 'chrome',
     //   },
     // },
-  ]
+  ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "pnpm run build && pnpm run dev",
-  //   port: 3000
-  // }
+  webServer: {
+    command: "pnpm run build && pnpm run cf-dev",
+    url: "http://localhost:3000",
+    timeout: 240 * 1000,
+    reuseExistingServer: !process.env.CI
+  }
 }
 
 export default config
