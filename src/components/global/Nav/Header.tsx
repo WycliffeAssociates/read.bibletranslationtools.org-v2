491112f46a5bd0b44946b52318e3545956891d0a
Will Kelly <67284402+wkelly17@users.noreply.github.com>
2023-05-25 11:15:35 -0500

    Read dev (#12)
    * let a repo specifcy a web font by readign arbitrary custom meta data in index.json
    
    * had wrong link base for menu items
    
    ---------
    
    Co-authored-by: Will Kelly <willkelly@Will-Kellys-Pro.local>

--- src/components/global/Nav/Header.tsx
+++ src/components/global/Nav/Header.tsx
@@ -101,14 +101,16 @@ export function UnwrappedHeader(props: HeaderProps) {
           data-js="mobileMenu"
           class={`${
             mobileMenuOpen() ? "block" : "hidden"
-          } absolute top-full  left-0 right-0  z-50 w-full flex-col bg-darkAccent pt-5 md:static md:flex md:w-auto md:flex-row`}
+          } absolute left-0  right-0 top-full  z-50 w-full flex-col bg-darkAccent pt-5 md:static md:flex md:w-auto md:flex-row`}
         >
           <ul class="flex flex-col ltr:pl-4 rtl:pr-4 md:flex-row">
             <Index each={props.menuItems}>
               {(menuItem) => {
                 return (
                   <li class="my-2 capitalize hover:text-secondary focus:text-secondary md:mx-4 md:my-0">
-                    {<a href={props.repoUrl}>{t(String(menuItem()))}</a>}
+                    <a href={`${props.linkBase}/${menuItem()}`}>
+                      {t(String(menuItem()))}
+                    </a>
                   </li>
                 )
               }}
